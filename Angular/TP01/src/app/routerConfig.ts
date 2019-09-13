96c95ca84bd28dd0fcb15800ad92d236b30148d5
maurogch <maurogch@gmail.com>
2019-09-13 16:25:42 -0300

    Add link to router

--- Angular/TP01/src/app/routerConfig.ts
+++ Angular/TP01/src/app/routerConfig.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CartComponent } from './cart/cart.component';
 
+// Router tutorial: https://www.codingame.com/playgrounds/8104/angular-router-tutorial
+
 const appRoutes: Routes = [
   { path: '', component: HomeComponent }, // Default component for router-outlet
   { path: 'home', component: HomeComponent },
