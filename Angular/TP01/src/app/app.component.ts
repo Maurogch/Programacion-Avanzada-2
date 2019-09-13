211bb96a5d2c60dd5e3c7799b5ef6383fa06f8ea
maurogch <maurogch@gmail.com>
2019-09-13 14:00:16 -0300

    Simple bootstrap for article list

--- Angular/TP01/src/app/app.component.ts
+++ Angular/TP01/src/app/app.component.ts
@@ -10,30 +10,34 @@ export class AppComponent {
   title = 'Lista de productos';
   articles = [];
 
-  constructor(){
-    let article1 = new Article();
-    let article2 = new Article();
-    let article3 = new Article();
-    let article4 = new Article();
-    let article5 = new Article();
-    
+  constructor() {
+    const article1 = new Article();
+    const article2 = new Article();
+    const article3 = new Article();
+    const article4 = new Article();
+    const article5 = new Article();
+
     article1.id = 1;
     article2.id = 2;
     article3.id = 3;
     article4.id = 4;
     article5.id = 5;
 
-    article1.name = "Smart TV Philips";
-    article2.name = "Aire Acondicionado Slip Midea";
-    article3.name = "El Caloventor Axel 1900W";
-    article4.name = "Termotanque a Gas Señoral";
-    article5.name = "Celular libre Motorola G7";
+    article1.name = 'Smart TV Philips';
+    article2.name = 'Aire Acondicionado Slip Midea';
+    article3.name = 'Caloventor Axel 1900W';
+    article4.name = 'Termotanque a Gas Señoral';
+    article5.name = 'Celular libre Motorola G7';
 
-    article1.description = "Televisor LED Full HD 43";
-    article2.description = "Cuenta con 3250 W Frío y 3050 W Calor , con eficiencia energética A (frío) y C (calefacción)";
-    article3.description = "Cuenta con 3 niveles de potencia, corte por sobre temperatura, motor silencioso, fusible térmico de protección y termostato regulable";
-    article4.description = "Termotanque a Gas con piloto analizador; se puede apoyar o colgar. Producto de alta eficiencia certificado bajo normativa";
-    article5.description = "El Celular libre Motorola G7 Power ice violet tiene una poderosa batería de 5.000 mAh, diseñada para durar hasta 55 horas con una sola carga";
+    article1.description = ' La serie Philips 5800 con Smart TV te ofrece todas tus aplicaciones y contenidos favoritos en una gran pantalla en Full HD';
+    article2.description =
+      'Cuenta con 3250 W Frío y 3050 W Calor , con eficiencia energética A (frío) y C (calefacción)';
+    article3.description =
+      'Cuenta con 3 niveles de potencia, corte por sobre temperatura, motor silencioso, fusible térmico de protección y termostato regulable';
+    article4.description =
+      'Termotanque a Gas con piloto analizador; se puede apoyar o colgar. Producto de alta eficiencia certificado bajo normativa';
+    article5.description =
+      'El Celular libre Motorola G7 Power ice violet tiene una poderosa batería de 5.000 mAh, diseñada para durar hasta 55 horas con una sola carga';
 
     article1.price = 22000;
     article2.price = 30000;
@@ -47,11 +51,11 @@ export class AppComponent {
     article4.stock = 100;
     article5.stock = 100;
 
-    article1.img = "../assets/art1.jpg";
-    article2.img = "../assets/art2.jpg";
-    article3.img = "../assets/art3.jpg";
-    article4.img = "../assets/art4.jpg";
-    article5.img = "../assets/art5.jpg";
+    article1.img = '../assets/art1.jpg';
+    article2.img = '../assets/art2.jpg';
+    article3.img = '../assets/art3.jpg';
+    article4.img = '../assets/art4.jpg';
+    article5.img = '../assets/art5.jpg';
 
     this.articles.push(article1);
     this.articles.push(article2);
--- Angular/TP01/src/app/app.module.ts
+++ Angular/TP01/src/app/app.module.ts
@@ -2,21 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
 import { NgxPopper } from 'angular-popper';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent
-  ],
-  imports: [
-    BrowserModule,
-    NgxPopper,
-    NgbModule
-  ],
+  declarations: [AppComponent, CartComponent],
+  imports: [BrowserModule, NgxPopper, NgbModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
