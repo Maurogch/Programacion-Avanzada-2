cf3c7ad79ba30f41b36d7e7dec127ac43d09a6b5
Mauro Guerreiro <maurogch@gmail.com>
2019-09-21 01:03:36 -0300

    Post index page ready

--- Javascript/TP6/script.js
+++ Javascript/TP6/script.js
@@ -5,28 +5,53 @@ function indexLoad() {
 async function asyncLoadPosts() {
   await loadPosts()
     .then(result => {
-      var posts = document.getElementsByClassName('post');
-
-      if (posts.length < 1) {
-        document.getElementById('noposts').removeAttribute('hidden');
-      }
-
       console.log(result);
       result.forEach(element => {
         let div = document.createElement('div');
-        div.classList.add('card');
-        div.classList.add('mb-4');
+        div.classList.add('card', 'mb-4', 'post');
 
-        //seguir hacendo append de los otros tags de post
-        //descartar todo iframe
+        let img = document.createElement('img');
+        img.classList.add('card-img-top');
+        img.src = 'https://picsum.photos/id/' + element.id + '/750/300';
+        img.alt = 'Cargando Imágen';
+        div.appendChild(img);
 
-        div.appendChild(iframe);
+        let div2 = document.createElement('div');
+        div2.classList.add('card-body');
+
+        let h2 = document.createElement('h2');
+        h2.classList.add('card-title');
+        h2.innerHTML = element.title;
+        div2.appendChild(h2);
+
+        let p = document.createElement('p');
+        p.classList.add('card-text');
+        p.innerHTML = element.body.substring(100, 0); //limit body to 100 chars
+        div2.appendChild(p);
+
+        let a = document.createElement('a');
+        a.classList.add('btn', 'btn-primary');
+        a.href = document.URL + '/post.html?id=' + element.id;
+        a.innerHTML = 'Seguir leyendo';
+        div2.appendChild(a);
+
+        div.appendChild(div2);
+
+        let div3 = document.createElement('div');
+        div3.classList.add('card-footer', 'text-muted');
+        div3.innerHTML = new Date(element.date).toUTCString;
+        div.appendChild(div3);
 
         document.getElementById('posts').appendChild(div);
       });
+
+      document.getElementById('loader').setAttribute('hidden', true);
+
+      noPosts();
     })
     .catch(err => {
       console.log(err);
+      alert('Error cargando la lista de posts, recargue la página por favor');
     });
 }
 
@@ -54,4 +79,12 @@ function loadPosts() {
 
     request.send();
   });
+}
+
+function noPosts() {
+  var posts = document.getElementsByClassName('post');
+
+  if (posts.length < 1) {
+    document.getElementById('noposts').removeAttribute('hidden');
+  }
 }
