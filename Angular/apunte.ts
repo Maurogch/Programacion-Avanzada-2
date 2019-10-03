1000a58f91f23d1c4654cd6daadd32297f6c7106
Mauro Guerreiro <maurogch@gmail.com>
2019-10-03 10:34:53 -0300

    apuntes

---
+++ Angular/apunte.ts
@@ -0,0 +1,23 @@
+//Importar jQuery, popper, bootstrap
+//yarn install jquery
+//yarn install popper.js
+//yarn install bootrsrap
+//add "./node_modules/jquery/dist/jquery.min.js",
+//    "./node_modules/popper.js/dist/umd/popper.min.js",
+//    "./node_modules/bootstrap/dist/js/bootstrap.min.js"
+// to angular.json, "scripts": []
+//add "./node_modules/bootstrap/dist/css/bootstrap.min.css"
+// to angular.json, "styles": []
+//add import * as $ from 'jquery'; in app.module.ts
+
+//import {FormsModule } from '@angular/forms'
+//es necesario para ngModel double binding en forms, a veces esta linea debe ser puesta manualmente
+
+//Comunicación entre componente cuando hay ruteo
+//----Servicios----
+
+//get parm passsed by route
+//route.snapshot.paramMap.get('param')
+
+//Agregar a tabla un boton para eliminar alumnos
+//Mostrar alumno va a un formulario de edición (con edicion posible)
