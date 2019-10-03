744573a2072ab0f28e186770946f3b363bad5e9d
Mauro Guerreiro <maurogch@gmail.com>
2019-10-03 10:35:30 -0300

    add redirect to router

--- Angular/TP01/src/app/router-config.module.ts
+++ Angular/TP01/src/app/router-config.module.ts
@@ -6,9 +6,10 @@ import { CartComponent } from './cart/cart.component';
 // Router tutorial: https://www.codingame.com/playgrounds/8104/angular-router-tutorial
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }, // Default component for router-outlet
+  { path: '', redirectTo: 'home', pathMatch: 'full'  }, // Default component for router-outlet
   { path: 'home', component: HomeComponent },
-  { path: 'cart', component: CartComponent }
+  { path: 'cart', component: CartComponent },
+  { path: '**', redirectTo: 'home', pathMatch: 'full'  } // Redirect to home on non existant path
 ];
 
 @NgModule({
