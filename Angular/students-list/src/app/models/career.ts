0227d172fc3b5be485525d6daa916b7e2bf720fd
maurogch <maurogch@gmail.com>
2019-10-11 16:16:50 -0300

    Refactored Clases

--- Angular/students-list/src/app/models/career.ts
+++ Angular/students-list/src/app/models/career.ts
@@ -1,4 +1,25 @@
 export class Career {
-  careerId: number;
-  name: string;
+  private _careerId: number;
+  private _name: string;
+
+  constructor(careerId: number, name?: string) {
+    this._careerId = careerId;
+    this._name = name;
+  }
+
+  public get careerId(): number {
+    return this._careerId;
+  }
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public set careerId(value: number) {
+    this._careerId = value;
+  }
+
+  public set name(value: string) {
+    this._name = value;
+  }
 }
