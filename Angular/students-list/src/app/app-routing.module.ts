e51e750f2919263a41644b138164e02436dc85f6
maurogch <maurogch@gmail.com>
2019-10-11 16:17:07 -0300

    add param to view route

--- Angular/students-list/src/app/app-routing.module.ts
+++ Angular/students-list/src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
-import { NgModule }              from '@angular/core';
-import { RouterModule, Routes }  from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { StudentAddComponent } from './components/student-add/student-add.component';
 import { StudentViewComponent } from './components/student-view/student-view.component';
 import { StudentListComponent } from './components/student-list/student-list.component';
 
 const appRoutes: Routes = [
   { path: 'add', component: StudentAddComponent },
-  { path: 'view',        component: StudentViewComponent },
-  { path: 'list',        component: StudentListComponent },  
-  { path: '',   redirectTo: '/list', pathMatch: 'full' }
+  { path: 'view/:id', component: StudentViewComponent },
+  { path: 'list', component: StudentListComponent },
+  { path: '', redirectTo: '/list', pathMatch: 'full' },
+  { path: '**', redirectTo: '/list', pathMatch: 'full' }
 ];
 
 @NgModule({
@@ -18,8 +19,6 @@ const appRoutes: Routes = [
       { enableTracing: true } // <-- debugging purposes only
     )
   ],
-  exports: [
-    RouterModule
-  ]
+  exports: [RouterModule]
 })
 export class AppRoutingModule {}
